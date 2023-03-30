c9375ce6bcea8b87e90835918e4cfe46947ab952
Haider959 <saochiha@gmail.com>
2023-03-30 12:18:24 +0300

    v0.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,32 @@
-import typescript from 'rollup-plugin-typescript2'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import postcss from 'rollup-plugin-postcss'
-import autoprefixer from 'autoprefixer'
-import { terser } from 'rollup-plugin-terser'
-import packageJson from './package.json'
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
+import { terser } from "rollup-plugin-terser";
+import packageJson from "./package.json";
 
 const config = {
-  input: 'src/index.tsx',
+  input: "src/index.tsx",
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript({
-      exclude: ['**/*.stories.tsx', '**/*.stories.mdx']
+      exclude: ["**/*.stories.tsx", "**/*.stories.mdx"]
     }),
     postcss({
-      modules: true,
-      extract: false,
-      minimize: true,
-      plugins: [autoprefixer()]
+      plugins: [],
+      minimize: true
     }),
     terser()
   ],
   output: {
     file: packageJson.main,
-    format: 'cjs',
+    format: "cjs",
     sourcemap: true
   }
-}
+};
 
-export default config
+export default config;
