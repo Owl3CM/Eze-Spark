0b29de78550ee9351beedee199c5163103ef2549
Owl3CM <saochiha@gmail.com>
2023-08-10 15:08:13 +0300

    v 0.0.32

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ import "./popup.css";
 export { ProviderContainer } from "./ProviderContainer";
 export { Popup, PopupMe } from "./ProviderController";
 export { PrintMe } from "./PrintMe";
+export * from "./types";
